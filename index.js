d7f06e3d6a52232b73a931c567a636bb53b52b20
Ankan703 <ankanchakraborty185@gmail.com>
2025-03-31 21:57:16 +0530

    index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello Ankan 69')
+  res.send('Hello Ankan')
 })
 
 app.get('/courses', function (req, res) {
